dbcd9fb45e185631b4c8f1cf9d547177addb84c8
thomastognacci <thomas.tognacci@gmail.com>
2018-11-14 16:25:08 -0330

    updated env.port

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.prepare()
     return handle(req, res)
   })
 
-  server.listen(3000, (err) => {
+  server.listen(process.env.PORT || 3000, (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
   })
