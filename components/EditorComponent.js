fc10bee38a0c280989fa14cfe0b49d3e61707144
thomastognacci <thomas.tognacci@gmail.com>
2018-12-06 23:59:19 -0330

    get editor state

--- components/EditorComponent.js
+++ components/EditorComponent.js
@@ -1,8 +1,9 @@
 import React, {Component} from "react";
 import dynamic from "next/dynamic";
-import {EditorState} from "draft-js";
+import {EditorState, convertToRaw} from "draft-js";
 import StyledEditor from "./styles/StyledEditor";
 import {uploadImageToFirebase} from "../lib/uploadToFirebase";
+import draftToHtml from "draftjs-to-html";
 
 const Editor = dynamic(import("react-draft-wysiwyg").then((module) => module.Editor), {
   ssr: false,
@@ -24,27 +25,35 @@ class EditorComponent extends Component {
   render() {
     const {editorState} = this.state;
     return (
-      <StyledEditor>
-        <Editor
-          editorState={editorState}
-          onEditorStateChange={this.onEditorStateChange}
-          toolbar={{
-            options: ["inline", "blockType", "image", "history"],
-            inline: {options: ["bold", "italic", "underline"]},
-            blockType: {
-              options: ["Normal", "H2", "H3"],
-            },
-            image: {
-              alignmentEnabled: false,
-              uploadCallback: uploadImageCallBack,
-              previewImage: true,
-              alt: false,
-              urlEnabled: false,
-              defaultSize: false,
-            },
-          }}
+      <>
+        <StyledEditor>
+          <Editor
+            editorState={editorState}
+            onEditorStateChange={this.onEditorStateChange}
+            toolbar={{
+              options: ["inline", "blockType", "image", "history"],
+              inline: {options: ["bold", "italic", "underline"]},
+              blockType: {
+                options: ["Normal", "H2", "H3"],
+              },
+              image: {
+                alignmentEnabled: false,
+                uploadCallback: uploadImageCallBack,
+                previewImage: true,
+                alt: false,
+                urlEnabled: false,
+                defaultSize: false,
+              },
+            }}
+          />
+        </StyledEditor>
+        <p>Current content:</p>
+        <textarea
+          disabled
+          style={{width: "100%", minHeight: "15rem"}}
+          value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
         />
-      </StyledEditor>
+      </>
     );
   }
 }
